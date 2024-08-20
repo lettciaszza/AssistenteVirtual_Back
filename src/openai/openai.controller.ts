164ff300d1c3a1fabdf7fb6df3e0c7a877d8e2c3
Mateus Henrique Ferraz <cabs_traces_0r@icloud.com>
2024-08-20 17:18:16 -0300

    add openai handler

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Req } from '@nestjs/common'
+import {
+    Controller,
+    HttpException,
+    HttpStatus,
+    Post,
+    Req,
+} from '@nestjs/common'
 import { Request } from 'express'
 import { OpenaiService } from './openai.service'
 
@@ -6,8 +12,23 @@ import { OpenaiService } from './openai.service'
 export class OpenaiController {
     constructor(private readonly openaiService: OpenaiService) {}
 
-    @Get()
-    findAll(@Req() request: Request): string {
-        return this.openaiService.getCompletion()
+    @Post()
+    async findAll(@Req() request: Request): Promise<string> {
+        if (!request.body.message)
+            throw new HttpException('Forbidden action', HttpStatus.FORBIDDEN)
+
+        try {
+            let completion = await this.openaiService.getCompletion(
+                request.body.message,
+            )
+
+            return completion
+        } catch (err) {
+            console.error(`openai::controller: ${err}`)
+            throw new HttpException(
+                err || 'Something went wrong',
+                HttpStatus.INTERNAL_SERVER_ERROR,
+            )
+        }
     }
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Param } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class OpenaiService {
-    getCompletion(): string {
-        return 'This action returns completion from openai'
+    constructor(private configService: ConfigService) {}
+    private async getOpenaiCompletion(message: string): Promise<string> {
+        const data = {
+            model: 'gpt-4o-mini',
+            messages: [{ role: 'user', content: message }],
+            temperature: 0.7,
+        }
+        const OPENAI_API_KEY = this.configService.get<string>('OPENAI_KEY')
+
+        try {
+            const response = await fetch(
+                'https://api.openai.com/v1/chat/completions',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${OPENAI_API_KEY}`,
+                    },
+                    body: JSON.stringify(data),
+                },
+            )
+
+            if (!response.ok) {
+                throw new Error(
+                    `Error: ${response.status} ${response.statusText}`,
+                )
+            }
+
+            const result = await response.json()
+            return result.choices[0].message.content
+        } catch (err) {
+            throw new Error(`Failed to get completion from OpenAI: ${err}`)
+        }
+    }
+
+    async getCompletion(message: string): Promise<string> {
+        try {
+            const response = await this.getOpenaiCompletion(message)
+            return response
+        } catch (err) {
+            throw err
+        }
     }
 }
