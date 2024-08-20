d54774fce32f84b798a773df4938b01e8504f8d9
Mateus Henrique Ferraz <cabs_traces_0r@icloud.com>
2024-08-20 17:17:37 -0300

    add env example and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { OpenaiController } from './openai/openai.controller';
-import { OpenaiService } from './openai/openai.service';
+import { OpenaiController } from './openai/openai.controller'
+import { OpenaiService } from './openai/openai.service'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-    imports: [],
+    imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
     controllers: [AppController, OpenaiController],
     providers: [AppService, OpenaiService],
 })
