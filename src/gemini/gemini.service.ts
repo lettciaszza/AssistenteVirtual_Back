37cb39ff2b2c76fb06f5b6d0444ec4387121cc5e
Mateus Henrique Ferraz <cabs_traces_0r@icloud.com>
2024-08-20 17:58:01 -0300

    removes unused import

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Param } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai'
 
