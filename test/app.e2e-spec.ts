061cf1282d948483fffa4b1250352d08895a23f1
Mateus Henrique Ferraz <cabs_traces_0r@icloud.com>
2024-08-21 10:33:40 -0300

    add e2e test for bearer token

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { ConfigService } from '@nestjs/config'
 
 describe('AppController (e2e)', () => {
     let app: INestApplication
@@ -19,10 +20,25 @@ describe('AppController (e2e)', () => {
         return request(app.getHttpServer()).get('/health').expect(200)
     })
 
-    it('/prompt (POST)', () => {
+    it('/prompt no auth (POST)', () => {
         const message = { message: 'Hello, world!' }
         return request(app.getHttpServer())
             .post('/prompt')
+            .send(message)
+            .expect(401)
+    })
+    it('/prompt with auth (POST)', () => {
+        const configService = new ConfigService()
+        const API_KEY = configService.get<string>('API_KEY')
+        const message = { message: 'Hello, world!' }
+        if (!API_KEY)
+            throw Error(
+                'No API_KEYS found on the enviroment, please check the .env',
+            )
+
+        return request(app.getHttpServer())
+            .post('/prompt')
+            .set('Authorization', API_KEY)
             .send(message)
             .expect(201)
     })
