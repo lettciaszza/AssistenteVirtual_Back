2ecd3f1dac383b7522b5dd56a49c4ec447a0b506
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-08-20 22:40:03 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,10 @@ import { AppService } from './app.service'
 import { GeminiController } from './gemini/gemini.controller'
 import { GeminiService } from './gemini/gemini.service'
 import { ConfigModule } from '@nestjs/config'
-import { OpenaiController } from './openai/openai.controller';
-import { OpenaiService } from './openai/openai.service';
 
 @Module({
     imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
-    controllers: [AppController, GeminiController, OpenaiController],
-    providers: [AppService, GeminiService, OpenaiService],
+    controllers: [AppController, GeminiController],
+    providers: [AppService, GeminiService],
 })
 export class AppModule {}
